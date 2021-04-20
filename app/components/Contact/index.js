a994f46caa18d4b743cb30990a9522cb4709b574
Rashid <rashidkhokhar98@gmail.com>
2021-04-20 22:08:19 +0500

    signup added

--- app/components/Contact/index.js
+++ app/components/Contact/index.js
@@ -42,29 +42,19 @@ function Contact() {
         <div className="section-title">
           <h2>Contact</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        
         </div>
+       
+        <div className=" row phone mt-2 pt-2" style={{backgroundColor: '#C2A835'}}>
+                <h4 className="ml-2">Call Us:</h4>
+                <p >+55 48 9101-3968</p>
+              </div>
+
+            
         <div className="row">
-          <div className="col-lg-5 d-flex align-items-stretch">
-            <div className="info">
-              <div className="address">
-                <i className="icofont-google-map" />
-                <h4>Location:</h4>
-                <p>Umer Street, Johar Town, Lahore</p>
-              </div>
-              <div className="email">
-                <i className="icofont-envelope" />
-                <h4>Email:</h4>
-                <p>casabio@example.com</p>
-              </div>
-              <div className="phone">
-                <i className="icofont-phone" />
-                <h4>Call:</h4>
-                <p>+92 3154518620</p>
-              </div>
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3402.7257551052526!2d74.28875801462978!3d31.47672940644618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391903e64da0e06d%3A0x739b1e1a979589ec!2sJamea%20Masjid%20Taqwa!5e0!3m2!1sen!2s!4v1618471783990!5m2!1sen!2s" frameBorder={0} style={{border: 0, width: '100%', height: 290}} allowFullScreen />
-            </div>
-          </div>
-          <div className="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+         
+           
+          
 
             <form onSubmit={handleSubmit(onSubmit)} className="php-email-form" >
             <div className="form-row mt-3 mb-4">
@@ -128,9 +118,12 @@ function Contact() {
                       Send Message
                     </button>
                   </div>
+                
+            
+          
             </form>
 
-          </div>
+        
         </div>
       </div>
     </section>
--- app/components/Navbar/index.js
+++ app/components/Navbar/index.js
@@ -12,22 +12,26 @@ function Navbar() {
   return (
     <>
   <header id="header" className="fixed-top ">
-  <div className="container d-flex align-items-center">
-  {/*<img src="https://i.ibb.co/zH7XBs1/logo.png" className="img-fluid" alt="logo" border="0" width="40" height="40"/>*/}
-    <h1 className="logo mr-auto"><a href="/">casabio</a></h1>
+  <div className="container-fluid d-flex align-items-center">
+  {/*<img src="https://i.ibb.co/zH7XBs1/logo.png" className="img-fluid" alt="logo" border="0" width="40" height="40"/>
+    <h1 className="logo mr-auto"><a href="/">casabio</a></h1>*/}
     {/* Uncomment below if you prefer to use an image logo 
     {/* <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" ></a>*/}
-    <nav className="nav-menu d-none d-lg-block">
+    <nav className="nav-menu d-none d-lg-block w-100">
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#Blogs">Blogs</a></li>
         <li><a href="#Products">Services & Products</a></li>
         <li><a href="#about">Volunteering </a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>{/* .nav-menu */}
-    <a href="/login" className="get-started-btn scrollto">Login</a>
+       <li><a href="#contact">Contact</a></li>
+      
+    {/* .nav-menu */}
+    
+    </ul>
+     </nav>
+     <a href="/login" className="get-started-btn scrollto">Login</a>
+   
   </div>
 </header>{/* End Header */}
 </>
